e8c42653298718f5a79e40c2a673aeb2e6a528ad
Christian Nilsson <christian.nilsson.4@wirelesscar.com>
2024-10-07 10:13:10 +0200

    Ec2RoleFix

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ provider "aws" {
   region = "us-west-2"
 }
 resource "aws_iam_role" "ec2_role" {
-  assume_role_policy = jsondecode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Action" : "sts:AssumeRole",
-        "Effect" : "Allow",
-        "Sid" : "",
-        "Principal" : { "Service" : "ec2.amazonaws.com" }
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Sid = "",
+        Principal = { Service = "ec2.amazonaws.com" }
       }
     ]
   })
